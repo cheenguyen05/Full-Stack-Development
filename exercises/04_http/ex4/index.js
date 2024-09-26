a8e9e4110bc5b7ce24120920accc96120e9f99e8
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-26 16:11:14 +0300

    trial

--- exercises/04_http/ex4/index.js
+++ exercises/04_http/ex4/index.js
@@ -1,16 +1,17 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
     let body = [];
-    request
-      .on('data', chunk => {
+
+    req.on('data', (chunk) => {
         body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).reverse().toString();
+        console.log("body",body)
+        res.write(body)
+        res.end()
       })
-      .on('end', () => {
-        body = Buffer.concat(body).toString();
-        response.write(body);
-        response.end();
-      });
+
 
 }).listen(3000);
