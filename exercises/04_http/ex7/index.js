fa204c78ec3f2e9a0dcc4c8010da887475c09c06
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-27 00:02:23 +0300

    solved

---
+++ exercises/04_http/ex7/index.js
@@ -0,0 +1,63 @@
+const http = require('http')
+const fs = require("fs");
+
+
+module.exports =  http.createServer((req,res)=>{
+    if(req.url === "/"){
+        fs.readFile("index.html",(err,pagersp)=>{
+            if(err){
+                res.writeHead(404);
+                res.write('Requested content not found',err)
+
+            }
+            else {
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write(pagersp)
+            }
+            res.end();
+        })
+    }
+    else if(req.url==="/classical"){
+        fs.readFile("homer.html",(err,pagersp)=>{
+            if(err){
+                res.writeHead(404);
+                res.write('Requested content not found',err)
+            }
+            else{
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write(pagersp);
+            }
+            res.end();
+        })
+    }
+    else if(req.url==="/dystopy"){
+        fs.readFile("bradbury.html",(err,pagersp)=>{
+            if(err){
+                res.writeHead(404);
+                res.write('Requested content not found',err)
+            }
+            else{
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write(pagersp);
+            }
+            res.end();
+        })
+    }
+    else if(req.url==="/ respond"){
+        fs.readFile("index.html",(err,pagersp)=>{
+            if(err){
+                res.writeHead(404);
+                res.write('Requested content not found',err)
+            }
+            else{
+                res.writeHead(200,{'Content-Type':'text.html'});
+                res.write(pagersp);
+            }
+            res.end();
+        })
+    }
+    else {
+        res.writeHead(404)
+        res.end()
+    }
+}).listen(3000)
