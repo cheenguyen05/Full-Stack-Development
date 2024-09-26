759bc21009c6d0555753cad0fe3404b18a69d57c
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-26 12:58:31 +0300

    fixed

--- exercises/04_http/ex3/index.js
+++ exercises/04_http/ex3/index.js
@@ -3,14 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer(function(request, response) {
-  fs.readFile(path.resolve('index.html'), function(error, htmlPage) {
-    if (error) {
-      response.writeHead(404);
-      response.write('An error occured: ', error);
-    } else {
-      response.writeHead(202, { 'Content-Type': 'text/html' });
-      response.write(htmlPage);
-    }
-    response.end();
-  });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      const hasContentType = response.hasHeader('content-type');
+      console.log("header", hasContentType)
+      response.write(JSON.stringify(request.headers))
+      response.end();
 }).listen(3000);
