cd09ec5898ac632b7eed2f031ce3ea65b24c31dc
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-26 22:45:56 +0300

    fix 404 error

--- exercises/04_http/ex6/index.js
+++ exercises/04_http/ex6/index.js
@@ -1,66 +1,51 @@
-const http = require('http')
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+// Tạo server HTTP
+http.createServer((request, response) => {
+    // Lấy path từ URL
+    const urlPath = request.url;
 
-const server = http.createServer((req,res)=>{
-    if(req.url === "/"){
-        fs.readFile("index.html",(err,pagersp)=>{
-            if(err){
-                res.writeHead(404);
-                res.write('Requested content not found',err)
-
+    // Định tuyến dựa trên path
+    if (urlPath === '/') {
+        // Đọc và gửi nội dung của file index.html
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('Requested content not found');
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(data);
             }
-            else {
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write(pagersp)
+        });
+    } else if (urlPath === '/classical') {
+        // Đọc và gửi nội dung của file homer.html
+        fs.readFile(path.join(__dirname, 'homer.html'), (err, data) => {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('Requested content not found');
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(data);
             }
-            res.end();
-        })
+        });
+    } else if (urlPath === '/dystopy') {
+        // Đọc và gửi nội dung của file bradbury.html
+        fs.readFile(path.join(__dirname, 'bradbury.html'), (err, data) => {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('Requested content not found');
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(data);
+            }
+        });
+    } else {
+        // Phản hồi với mã lỗi 404 nếu không tìm thấy đường dẫn
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('Requested content not found');
     }
-    else if(req.url==="/classical"){
-        fs.readFile("homer.html",(err,pagersp)=>{
-            if(err){
-                res.writeHead(404);
-                res.write('Requested content not found',err)
-            }
-            else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write(pagersp);
-            }
-            res.end();
-        })
-    }
-    else if(req.url==="/dystopy"){
-        fs.readFile("bradbury.html",(err,pagersp)=>{
-            if(err){
-                res.writeHead(404);
-                res.write('Requested content not found',err)
-            }
-            else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write(pagersp);
-            }
-            res.end();
-        })
-    }
-    else if(req.url==="/ respond"){
-        fs.readFile("index.html",(err,pagersp)=>{
-            if(err){
-                res.writeHead(404);
-                res.write('Requested content not found',err)
-            }
-            else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write(pagersp);
-            }
-            res.end();
-        })
-    }
-    else {
-        res.writeHead(404)
-        res.write('Requested content not found')
-        res.end()
-    }
-})
-
-server.listen(3000)
+}).listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
