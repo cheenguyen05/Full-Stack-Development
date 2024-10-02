b8d1ab208cd10ef3d394db5c2f2c64a7057324b4
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-10-02 15:43:53 +0300

    getcookie

--- exercises/05_cookie/ex3/script.js
+++ exercises/05_cookie/ex3/script.js
@@ -1,10 +1,25 @@
 // TODO: Copy the setCookies function from the previous exercise
 function setCookies() {
-  
+  // TODO: Get form values. getElementById() might be useful here.
+  const text1_value = document.getElementById("text1").value;
+  const text2_value = document.getElementById("text2").value;
+  const checkbox = document.getElementById("checkbox").checked;
+  // TODO: Set cookie for each form value.
+  document.cookie = 'text1=${text1_value}; path=/';
+  document.cookie = 'text2=${text2_value}; path=/';
+  document.cookie = 'checkbox=${checkbox}; path=/';
+
+  alert('Cookies set!');
 }
+
 
 // TODO: Implement the getCookie function. It should take a cookie name as an argument and return the cookie value.
 function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null; // Return null if cookie is not found
+        
 
 }
 
