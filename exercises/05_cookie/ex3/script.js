1901a4bb14ad985c4436d58984c50cd96e6043de
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-10-02 15:49:08 +0300

    getcookie2

--- exercises/05_cookie/ex3/script.js
+++ exercises/05_cookie/ex3/script.js
@@ -5,11 +5,10 @@ function setCookies() {
   const text2_value = document.getElementById("text2").value;
   const checkbox = document.getElementById("checkbox").checked;
   // TODO: Set cookie for each form value.
-  document.cookie = 'text1=${text1_value}; path=/';
-  document.cookie = 'text2=${text2_value}; path=/';
-  document.cookie = 'checkbox=${checkbox}; path=/';
+  document.cookie = `text1=${text1_value}; path=/`;
+  document.cookie = `text2=${text2_value}; path=/`;
+  document.cookie = `checkbox=${checkbox}; path=/`;
 
-  alert('Cookies set!');
 }
 
 
