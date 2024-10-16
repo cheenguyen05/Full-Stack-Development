2725e49d66d5366f8626b1ad0744bb9d17edc228
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-10-16 16:22:18 +0300

    ex2

--- exercises/07_security/ex2/index.js
+++ exercises/07_security/ex2/index.js
@@ -17,61 +17,57 @@ const server = http.createServer((req, res) => {
       This syntax enables using the defined CORS headers with  writeHead() method in the TODOs below. See writeHead() method parameters: (https://nodejs.org/api/http.html#http_response_writehead_statuscode_statusmessage_headers).
   */
   const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "HEAD, GET, POST",
+    "Access-Control-Max-Age": 1440,
 
   };
 
   let filePath = path.join(__dirname, 'index.html');
   let stat = fs.statSync(filePath);
 
-  // The three lines of code below, ending with 'res.end(data);',
-  // are here to enable testing
-  // the server and CORS headers, when you seek to remove
-  // the CORS error shown in the browser DevTools.
-  //
-  // IMPORTANT! DELETE THE THREE LINES BELOW ENDING WITH 'res.end(data);', 
-  // BUT only after you
-  // have already defined a working set of CORS headers above. So,
-  // remove the lines after you have completed the TODO above, but 
-  // before you start to write your own code for the TODOs below.
-  res.writeHead(200, headers);
-  const data = fs.readFileSync(filePath, 'utf8');
-  res.end(data);
-
   // TODO 2: check that Origin header is set in all incoming requests
   // You can access the header with req.headers['origin']
   // You can check if a header is present in request headers with if(!req.headers['yourHeaderNameHere']){..
-
+  if(!req.headers['origin']) {
+    res.writeHead(400, headers); //400 bad request if no origin header
+    res.end('Origin header is missing');
+    return;
+  }
 
   // TODO 3: handle GET and POST HTTP methods
   // You can use req.method to access the request method 
   // remember to add CORS headers to response, you can use writeHead() here 
+  if (req.method === 'GET') {
+    res.writeHead(200, headers);
+    const data = fs.readFileSync(filePath, 'utf8');
+    res.end(data);
+    return;
+  } else if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;chunk
+    });
+    req.on('end', () => {
+      res.writeHead(200, headers);
+      res.end('Received POST data: ${body}');
+    });
+    return;
+  }
 
   // TODO 4: handle HEAD HTTP method, 
   // remember to add CORS headers to response
-
+  else if (req.method === 'HEAD') {
+    res.writeHead(200, headers);
+    res.end(); // No content for HEAD requests
+    return;
+  } else {
   // TODO 5: handle HTTP methods that are not allowed, 
   // remember to add CORS headers to response
-
-  // HINT: remember to use end() method of the response when you are ready to send them. If you are using if-else statements, place 
-  // "return;" 
-  // as the last line of all if-else branches. 
-  // So, something like:
-  // if(condition){
-  //     ....
-  //     response.end();
-  //     return;
-  // }
-  // else if(condition){
-  //     ....
-  //     response.end();
-  //     return;
-  // }
-  // else{
-  //     ....
-  //     response.end();
-  //     return;
-  // }
-
+  res.writeHead(405, headers);
+  res.end(`Method ${req.method} not allowed`);
+  return;
+  }
 
 });
 
