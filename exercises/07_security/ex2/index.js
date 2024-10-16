f603a9ceda220b15cf53579c1e762f4d619e1f13
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-10-16 16:35:50 +0300

    fix again

--- exercises/07_security/ex2/index.js
+++ exercises/07_security/ex2/index.js
@@ -8,44 +8,41 @@ const server = http.createServer((req, res) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, HEAD',
-    'Access-Control-Allow-Headers': '14400'
+    'Access-Control-Max-Age': '14400' // Cache CORS preflight response for 4 hours
   };
-
-  let filePath = path.join(__dirname, 'index.html');
-  let stat = fs.statSync(filePath);
 
   // TODO 2: Check if Origin header is set
   if (!req.headers['origin']) {
     res.writeHead(400, headers);
-    res.end('Origin header is missing');
+    res.end('Origin header not in the request'); // Adjusted message
     return;
   }
 
-  // TODO 3, 4, 5: Handle HTTP methods
+  // TODO 3: Handle GET request
   if (req.method === 'GET') {
     res.writeHead(200, headers);
-    const data = fs.readFileSync(filePath, 'utf8');
-    res.end(data);
+    res.end('I was requested using CORS!'); // Fixed expected response
     return;
-  } else if (req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk;
-    });
-    req.on('end', () => {
-      res.writeHead(200, headers);
-      res.end(`Received POST data: ${body}`);
-    });
-    return;
-  } else if (req.method === 'HEAD') {
+  } 
+  // TODO 4: Handle POST request
+  else if (req.method === 'POST') {
     res.writeHead(200, headers);
-    res.end(); // No content for HEAD
-    return;
-  } else {
-    res.writeHead(405, headers); // Method not allowed
-    res.end(`Method ${req.method} not allowed`);
+    res.end('I was requested using CORS!'); // Fixed expected response
     return;
   }
+  // TODO 5: Handle HEAD request
+  else if (req.method === 'HEAD') {
+    res.writeHead(200, headers);
+    res.end(); // No body content for HEAD requests
+    return;
+  } 
+  // Handle unsupported methods
+  else {
+    res.writeHead(405, headers);
+    res.end('Request used a HTTP method which is not allowed.'); // Adjusted message
+    return;
+  }
+
 });
 
 // DO NOT MODIFY BELOW THIS LINE
