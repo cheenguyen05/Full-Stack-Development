3bd48ccb7e69ac8ee7d7bc5c14247ea717bdc389
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-11-07 13:20:26 +0200

    mongoose schema

--- exercises/09_data_persistence/models/Event.js
+++ exercises/09_data_persistence/models/Event.js
@@ -2,6 +2,27 @@ const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
     //TODO: Fill in the schema
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minlen: 1,
+        maxlen: 50
+    },
+    date: {
+        type: String,
+        required: [true, 'Date is required!']
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ['planned', 'completed', 'cancelled', 'rejected'],
+            message: 'Status must be either planned, completed, cancelled, or rejected!'
+        },
+        default: 'planned'
+    },
+    description: {
+        type: String,
+    }
 });
 
 const Event = mongoose.model('Event', eventSchema);
